cb9994fae8072bc7bd38a64bb2e2d00339f4b7ef
Stefanos Karasavvidis <sk@karasavvidis.gr>
2019-03-29 15:39:27 +0200

    update extension description

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,7 +1,7 @@
 <?php
 $EM_CONF[$_EXTKEY] = [
     'title' => 'SK Video Content Element',
-    'description' => 'TYPO3 plugin to cache thumbnails of youtube/vimeo videos and play them after user confirmation. Responsive display. Works with css_styled_content and fluid_styled_content. No other dependencies',
+    'description' => 'TYPO3 content element to cache thumbnails of youtube/vimeo videos and play them after user confirmation. Responsive display. Works with css_styled_content and fluid_styled_content. No other dependencies.',
     'category' => 'fe',
     'author' => 'Stefanos Karasavvidis',
     'author_email' => 'sk@karasavvidis.gr',
