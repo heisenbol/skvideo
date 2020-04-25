a83d881fa0a557b6dc5927577a27b93e27a08fd7
Stefanos Karasavvidis <sk@karasavvidis.gr>
2020-04-25 16:22:47 +0300

    fix for ext_localconf

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,13 +1,15 @@
 <?php
 defined('TYPO3_MODE') || die();
 
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['tt_content_drawItem']['skvideo_skvideo_ce'] =
-   \Skar\Skvideo\Hooks\PageLayoutView\SkvideoPreviewRenderer::class;
+$boot = function () {
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['tt_content_drawItem']['skvideo_skvideo_ce'] =
+       \Skar\Skvideo\Hooks\PageLayoutView\SkvideoPreviewRenderer::class;
 
+    // declare cache for video names and titles
+    if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][\Skar\Skvideo\Helper::TITLES_CACHE_NAME])) {
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][\Skar\Skvideo\Helper::TITLES_CACHE_NAME] = array();
+    }
+};
 
-   
-   
-// declare cache for video names and titles
-if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][\Skar\Skvideo\Helper::TITLES_CACHE_NAME])) {
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][\Skar\Skvideo\Helper::TITLES_CACHE_NAME] = array();
-}   
+$boot();
+unset($boot);
