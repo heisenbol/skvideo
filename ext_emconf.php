db92d362b5ab36973358e80b85be5aa4ceb0e6d1
Stefanos Karasavvidis <sk@karasavvidis.gr>
2020-05-08 17:34:43 +0300

    remove reference to css_styled_content from description

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,7 +1,7 @@
 <?php
 $EM_CONF[$_EXTKEY] = [
     'title' => 'SK Video Content Element',
-    'description' => 'TYPO3 content element to cache thumbnails of Youtube & Vimeo videos and play them after user confirmation. Options to remember user decision via cookie. Responsive display. Works with css_styled_content and fluid_styled_content. No other dependencies.',
+    'description' => 'TYPO3 content element to cache thumbnails of Youtube & Vimeo videos and play them after user confirmation. Options to remember user decision via cookie. Responsive display. Works with and without fluid_styled_content. No other dependencies.',
     'category' => 'fe',
     'author' => 'Stefanos Karasavvidis',
     'author_email' => 'sk@karasavvidis.gr',
