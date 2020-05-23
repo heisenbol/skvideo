00a7b16b8f0c3f25d1e5595b6fca3318fa29ab4a
Stefanos Karasavvidis <sk@karasavvidis.gr>
2020-05-23 07:51:53 +0300

    remove obsolete code

--- Classes/ExtensionConfiguration.php
+++ Classes/ExtensionConfiguration.php
@@ -8,17 +8,10 @@ class ExtensionConfiguration
 {
     public static function getSetting($key, $default = NULL) {
 
-        if (class_exists('TYPO3\CMS\Core\Configuration\ExtensionConfiguration')) {
-            $extensionConfiguration = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
-                \TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class
-            );
-            $backendConfiguration = $extensionConfiguration->get('skvideo');
-        } else {
-            $backendConfiguration = $GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['skvideo'];
-            if (!is_array($backendConfiguration)) {
-                $backendConfiguration = unserialize($backendConfiguration);
-            }
-        }
+        $extensionConfiguration = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
+            \TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class
+        );
+        $backendConfiguration = $extensionConfiguration->get('skvideo');
         $setting = $backendConfiguration[$key]??$default;
         if ($setting) { // check here also that it is not empty
             return $setting;
